89fab1c05a78f86c0041be91a70966eb3c60c52a
iceddrop <93996532+iceddrop@users.noreply.github.com>
2023-08-03 11:36:42 +0100

    fixed the nav bug

--- page.js
+++
@@ -1,7 +0,0 @@
-const activePage = window.location.pathname;
-console.log(activePage);
-const navLinks = document.querySelectorAll("nav a").forEach(link => {
-    if (link.href.includes(`${activePage}`)) {
-        link.classList.add("active");
-    } 
- })
